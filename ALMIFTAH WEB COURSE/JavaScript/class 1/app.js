a1001ae05156fd7f335d335d759fcd763cc6cf89
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-03 18:49:36 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 1/app.js
@@ -0,0 +1,13 @@
+// alert("Are You Sure?")
+
+
+let add = 45 + 75
+let min = 75 - 45
+let multi = 45 * 75
+let div = 45 / 75
+
+// console.log(add, min, multi, div)
+console.log("SUM", add)
+console.log("Subtract",min)
+console.log("Multiply",multi)
+console.log("div")
