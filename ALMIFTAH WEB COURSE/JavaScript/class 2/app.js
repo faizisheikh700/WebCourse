02bc28463b0530971ad4bc56febef6464c3fad6f
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-07 18:59:21 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 2/app.js
@@ -0,0 +1,30 @@
+// alert("I am ok")
+
+// let a = 3
+
+
+// a = 8
+
+// const a = 10
+
+// a = 15
+
+// console.log(a)
+
+
+
+
+
+
+// var age =15
+
+// age = 70
+// console.log(age)
+
+let  a = '1 '
+let b = 2
+let c = 6
+
+let sum = a + b + c
+
+console.log(sum)
