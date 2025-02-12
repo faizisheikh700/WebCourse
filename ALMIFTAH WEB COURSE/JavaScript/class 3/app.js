fa4c8179b5d1d1fe4c2438a588b98a39c7ee2a72
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-12 19:00:02 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 3/app.js
@@ -0,0 +1,54 @@
+
+// Data Types
+// primitives types
+// Number, String, Boolean, null, undefined, BigInt, Symbol
+
+//! let firstNumber = 230
+
+// let firstName = "JAVASCRIPT"
+
+// let discount = true
+
+// let value = null
+
+// let a;
+
+// console.log(a)
+
+// const Obj = {
+
+    // key: value
+
+//     name: "Ali",
+//     age: 16,
+//     pass: true,
+//     phoneNmuber: null
+
+// };
+
+// console.log(Obj.age);
+// console.log(Obj['age']);
+
+// const Practice = {
+//     name: "Ali",
+//     post: 500,
+//     follower: 1000,
+//     isFollow: false
+// };
+
+// console.log(Practice);
+
+// camelcase
+
+// let firstName = 62376;
+
+// snackcase
+
+// let frist_Name = 123;
+
+// kababcase
+
+// let first-Name = 13334;
+
+// pascalcase
+// let FirstName = 1234
---
+++ ALMIFTAH WEB COURSE/JavaScript/class 4/app.js
@@ -0,0 +1,18 @@
+// const Obj ={
+//     name: "Karchi",
+//     code: 75850,
+//     light: null
+// }
+
+// console.log(Obj.name)
+
+
+
+
+
+//! Loop
+
+// for (i = 1; i >= 0; i++){
+    
+//     console.log(i)
+// }
