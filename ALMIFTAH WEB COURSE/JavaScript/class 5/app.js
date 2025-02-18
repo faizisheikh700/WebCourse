2daf6bf31c16cca239eb88a50bec626cb97f24ed
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-19 02:19:13 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 5/app.js
@@ -0,0 +1,21 @@
+//!Loop
+
+//? for loop
+
+// for(i = 0; i >= 10; i++){
+//     console.log(i)
+// }
+
+// !infinite loop / never ending loop
+
+for (i = 1, i >= 0, i++){
+    console.log(i)
+}
+
+//? while loop
+
+// let j= 0
+// while(j < 20){
+//     j++
+//     console.log(j)
+// }
---
+++ ALMIFTAH WEB COURSE/JavaScript/class 6/app.js
@@ -0,0 +1,26 @@
+// loop
+
+//! do while loop
+
+// let i = 0
+
+// do{
+//     i++
+//     console.log(i)
+// }while(i<5);
+
+
+//!while loop
+
+// let i = 0;
+// while(i<6){
+//     i++
+//     console.log(i)
+// };
+
+// const obj = {
+//     name: "sakib",
+//     age: 25,
+//     job: "developer"
+// }
+// console.log(obj)
