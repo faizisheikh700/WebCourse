ffadbe9ad5db22273888a7984d09bf503e8e148d
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-19 19:04:38 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 7/app.js
@@ -0,0 +1,14 @@
+const arr = ['Ali', 'Raza', 'Khan', 'Anas'];
+
+// console.log(arr);
+
+// arr.push('Majeed', 'Ahmed', 'Faizan');
+arr.pop();
+console.log(arr);
+
+
+
+// for(let i =0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
