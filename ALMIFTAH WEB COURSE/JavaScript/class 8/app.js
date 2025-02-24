7b8150bd20a4d43bfa347765014f5bc7a5322063
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-24 19:00:17 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/class 8/app.js
@@ -0,0 +1,33 @@
+// let time = "11am";
+
+// if (time == "10am") {
+//   console.log("HELLO SHAHEER GOOD MORNING");
+// } else {
+//   console.log("good night");
+// }
+
+// let day = "2ndday";
+
+// if (day == "2ndday"){
+//     // console.log("monday")
+//     alert("Hello Monday")
+// }
+
+
+
+
+let a = 10;
+let b = 30;
+
+let sum = a + b;
+console.log(sum);
+
+
+if (sum < 40){
+    console.log("30 is less than 40")
+}else{
+    console.log("30 is greater than 40")
+}
+
+
+
---
+++ ALMIFTAH WEB COURSE/JavaScript/class 9/app.js
@@ -0,0 +1,38 @@
+// let time = "11am"
+
+// if (time === "10am") {
+//   console.log("Good Morning")
+// }else {
+//   console.log("Good Night")
+// }
+
+// let day = "saturday";
+
+// switch ("saturday") {
+//   case "Sunday":
+//     day = "This is the holy day";
+//     break;
+//   case "Monday":
+//     day = "Eat day";
+//     break;
+//   default:
+//     console.log("Invalid day");
+// }
+
+// console.log(day);
+
+
+
+
+
+function myFirst(){
+ console.log("Hello World"); 
+};
+
+myFirst();  // call ya Invoke
+
+function my2nd(){
+ return  2 + 5 
+}
+console.log(my2nd())
+
