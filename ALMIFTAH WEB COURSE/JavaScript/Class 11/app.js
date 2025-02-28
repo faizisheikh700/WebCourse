9b7d5f2ab87038dfd7fe494058bfa4903058e028
faizisheikh700 <faizisheikh700@gmail.com>
2025-02-28 18:53:29 +0500

    Teach

---
+++ ALMIFTAH WEB COURSE/JavaScript/Class 11/app.js
@@ -0,0 +1,11 @@
+let time = "10am"
+
+console.log(time)
+
+document.write(time)
+
+
+document.getElementById('txt').innerHTML = "I am Ali"
+
+
+document.getElementById('image').src = "https://fromyouflowers.pk/wp-content/uploads/2023/07/1000x1000-standard-fruit-basket-500x500-1.jpg"
---
+++ ALMIFTAH WEB COURSE/JavaScript/class 10/app.js
@@ -0,0 +1,14 @@
+function myFunction(){
+    console.log("Hello world");
+}
+myFunction();
+
+function voice(a, b, c, d) {
+    return a * b - c / d;
+    //     6 * 2 - 14 / 7
+    //     6 * 2 - 2
+    //     12 - 2
+    //     10
+}
+
+document.write(window)
