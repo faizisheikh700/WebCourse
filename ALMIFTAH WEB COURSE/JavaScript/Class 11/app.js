2586627e5ff85120993bca73a768d9aeb3cddbf7
faizisheikh700 <faizisheikh700@gmail.com>
2025-03-03 23:45:14 +0500

    Teach

--- ALMIFTAH WEB COURSE/JavaScript/Class 11/app.js
+++ ALMIFTAH WEB COURSE/JavaScript/Class 11/app.js
@@ -1,11 +1,16 @@
-let time = "10am"
+let time = "10am";
 
-console.log(time)
+console.log(time);
 
-document.write(time)
+document.write(time);
+
+document.getElementById("txt").innerHTML = "I am Ali";
+
+document.getElementById("image").src =
+  "https://fromyouflowers.pk/wp-content/uploads/2023/07/1000x1000-standard-fruit-basket-500x500-1.jpg";
 
 
-document.getElementById('txt').innerHTML = "I am Ali"
 
-
-document.getElementById('image').src = "https://fromyouflowers.pk/wp-content/uploads/2023/07/1000x1000-standard-fruit-basket-500x500-1.jpg"
+  function changeImage() {
+    document.getElementById("myImage").src = "<div className="" />Rectangle.png";
+}
